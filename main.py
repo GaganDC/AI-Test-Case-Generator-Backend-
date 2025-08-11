cef50855df07318d66b34c6c9f7509ce14cfadc6
Gagandeep <gagandeep_c@outlook.com>
2025-08-11 10:18:50 +0530

    Updated the frontend api

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://ai-test-case-generator-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
